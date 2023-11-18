13773f4c718e85685f77bcf175e87fb182d74989
Julian Javier Pimeinta Rendon <jpimientarm56@gmail.com>
2023-11-18 10:36:56 -0700

    poop

--- src/Mega2560_Firmware/src/main.cpp
+++ src/Mega2560_Firmware/src/main.cpp
@@ -3,16 +3,19 @@
 // put function declarations here:
 int myFunction(int, int);
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // new function  // put your main code here, to run repeatedly:
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, int y)
+{
   return x + y;
 }
