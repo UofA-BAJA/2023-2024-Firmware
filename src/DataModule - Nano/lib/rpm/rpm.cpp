39ebd26c4427c2450e53fcdd2a786022c033b37d
Julian Javier Pimeinta Rendon <jpimientarm56@gmail.com>
2024-01-15 13:29:20 -0700

    RPM go brrr

--- src/DataModule - Nano/lib/rpm/rpm.cpp
+++ src/DataModule - Nano/lib/rpm/rpm.cpp
@@ -7,29 +7,30 @@
 #include "config.h"
 #include "datamodule.h"
 
-#define RPM_SENSING_DURATION_PERIOD_MS 20 //time in ms for sensor to collect
-#define RPM_NUM_OF_PERIODS_TO_AVG 20 //number of periods to average for rpm calculation
-
+#define RPM_SENSING_DURATION_PERIOD_MS 20 // time in ms for sensor to collect
+#define RPM_NUM_OF_PERIODS_TO_AVG 20      // number of periods to average for rpm calculation
+#define NUM_MAGNETS 30                    // number of magnets on the wheel
 
 int rpm_left_counts[RPM_NUM_OF_PERIODS_TO_AVG] = {0};
+int rpm_rear_counts[RPM_NUM_OF_PERIODS_TO_AVG] = {0};
 int rpm_right_counts[RPM_NUM_OF_PERIODS_TO_AVG] = {0};
 
-
-volatile int RPM_DataModule::left_rpm_counter = 0; // Define and initialize the static member
+volatile int RPM_DataModule::left_rpm_counter = 0;  // Define and initialize the static member
 volatile int RPM_DataModule::right_rpm_counter = 0; // Define and initialize the static member
-
-void RPM_DataModule::data_module_initialization_procedure() {
+volatile int RPM_DataModule::rear_rpm_counter = 0;  // Define and initialize the static member
+void RPM_DataModule::data_module_initialization_procedure()
+{
     sei(); // Enable interrupts
     initialize_left_rpm_sensor();
     initialize_right_rpm_sensor();
 
-    #if DEBUG_LEVEL == DEV
-        Serial.println("initialized RPM data module");
-    #endif
-    
+#if DEBUG_LEVEL == DEV
+    Serial.println("initialized RPM data module");
+#endif
 }
 
-void RPM_DataModule::data_module_operating_procedure() {
+void RPM_DataModule::data_module_operating_procedure()
+{
 
     while (1)
     {
@@ -43,75 +44,104 @@ void RPM_DataModule::data_module_operating_procedure() {
         - write to sd card
         */
         _delay_ms(RPM_SENSING_DURATION_PERIOD_MS);
+        rear_rpm_counter = left_rpm_counter + right_rpm_counter;
 
-        shift_data_array_left(rpm_left_counts, RPM_NUM_OF_PERIODS_TO_AVG);
-        rpm_left_counts[RPM_NUM_OF_PERIODS_TO_AVG - 1] = left_rpm_counter;
+        shift_data_array_left(rpm_rear_counts, RPM_NUM_OF_PERIODS_TO_AVG);
+        rpm_rear_counts[RPM_NUM_OF_PERIODS_TO_AVG - 1] = rear_rpm_counter;
+        rear_rpm_counter = 0;
+
         left_rpm_counter = 0;
-
-        left_rpm = get_average_of_data_array(rpm_left_counts, RPM_NUM_OF_PERIODS_TO_AVG);
-
-        shift_data_array_left(rpm_right_counts, RPM_NUM_OF_PERIODS_TO_AVG);
-        rpm_right_counts[RPM_NUM_OF_PERIODS_TO_AVG - 1] = right_rpm_counter;
         right_rpm_counter = 0;
 
-        right_rpm = get_average_of_data_array(rpm_right_counts, RPM_NUM_OF_PERIODS_TO_AVG);
+        avg_rear_rpm_count = get_average_of_data_array(rpm_rear_counts, RPM_NUM_OF_PERIODS_TO_AVG);
 
-        #if DEBUG_LEVEL == DEV
-            Serial.print(">left_rpm: ");
-            Serial.println(left_rpm);
-            Serial.print(">right_rpm: ");
-            Serial.println(right_rpm);
-        #endif
+        rear_rpm = calculate_rpm(avg_rear_rpm_count);
+
+        // shift_data_array_left(rpm_left_counts, RPM_NUM_OF_PERIODS_TO_AVG);
+        // rpm_left_counts[RPM_NUM_OF_PERIODS_TO_AVG - 1] = left_rpm_counter;
+        // left_rpm_counter = 0;
+
+        // avg_left_rpm_count = get_average_of_data_array(rpm_left_counts, RPM_NUM_OF_PERIODS_TO_AVG);
+
+        // left_rpm = calculate_rpm(avg_left_rpm_count);
+
+        // shift_data_array_left(rpm_right_counts, RPM_NUM_OF_PERIODS_TO_AVG);
+        // rpm_right_counts[RPM_NUM_OF_PERIODS_TO_AVG - 1] = right_rpm_counter;
+        // right_rpm_counter = 0;
+
+        // avg_right_rpm_count = get_average_of_data_array(rpm_right_counts, RPM_NUM_OF_PERIODS_TO_AVG);
+
+        // right_rpm = calculate_rpm(avg_right_rpm_count);
+
+#if DEBUG_LEVEL == DEV
+        Serial.print(">rear_rpm: ");
+        Serial.println(rear_rpm);
+        // Serial.print(">left_rpm: ");
+        // Serial.println(left_rpm);
+        // Serial.print(">right_rpm: ");
+        // Serial.println(right_rpm);
+#endif
     }
-    
 }
 
-void RPM_DataModule::initialize_left_rpm_sensor() {
+void RPM_DataModule::initialize_left_rpm_sensor()
+{
 
     // setting up rear left rpm sensor
-    DDRD &= ~(1 << DDD2); // set direction for input
-    PORTD |= (1 << PORTD2);  // enable the pullup resistor for stable input
-    
+    DDRD &= ~(1 << DDD2);   // set direction for input
+    PORTD |= (1 << PORTD2); // enable the pullup resistor for stable input
+
     EICRA |= (1 << ISC01) | (1 << ISC00); // set INT0 to trigger on rising edge
-    EIMSK |= (1 << INT0); // enable external interrupt for INT0
+    EIMSK |= (1 << INT0);                 // enable external interrupt for INT0
 }
 
-void RPM_DataModule::initialize_right_rpm_sensor() {
+void RPM_DataModule::initialize_right_rpm_sensor()
+{
 
     // setting up rear right rpm sensor
-    DDRD &= ~(1 << DDD3); // set direction for input
-    PORTD |= (1 << PORTD3);  // enable the pullup resistor for stable input
-    
+    DDRD &= ~(1 << DDD3);   // set direction for input
+    PORTD |= (1 << PORTD3); // enable the pullup resistor for stable input
+
     EICRA |= (1 << ISC11) | (1 << ISC10); // set INT1 to trigger on rising edge
-    EIMSK |= (1 << INT1); // enable external interrupt for INT1
+    EIMSK |= (1 << INT1);                 // enable external interrupt for INT1
 }
 
-void RPM_DataModule::shift_data_array_left(int data_array[], int array_size) {
-    for (int i = 0; i < array_size - 1; i++) {
+void RPM_DataModule::shift_data_array_left(int data_array[], int array_size)
+{
+    for (int i = 0; i < array_size - 1; i++)
+    {
         data_array[i] = data_array[i + 1];
     }
 }
 
-float RPM_DataModule::get_average_of_data_array(int data_array[], int array_size) {
+float RPM_DataModule::get_average_of_data_array(int data_array[], int array_size)
+{
     float sum = 0.0;
-    for (int i = 0; i < array_size; i++) {
+    for (int i = 0; i < array_size; i++)
+    {
         sum += data_array[i];
     }
     return sum / (float)array_size;
 }
-// float calculate_rpm(int rpm_count) {
+float RPM_DataModule::calculate_rpm(float sensor_trigger_count)
+{
 
-//     float rpm; 
+    float rpm;
+    float rotations;
 
-//     rpm = (rpm_count * 60) / (RPM_SENSING_DURATION_MS / 1000.0);
+    rotations = sensor_trigger_count / NUM_MAGNETS;
 
-//     return rpm;
-// }
+    rpm = (rotations / ((RPM_SENSING_DURATION_PERIOD_MS) / 1000.0)) * 60;
 
-ISR (INT0_vect) {
+    return rpm;
+}
+
+ISR(INT0_vect)
+{
     RPM_DataModule::left_rpm_counter++;
 }
 
-ISR (INT1_vect) {
+ISR(INT1_vect)
+{
     RPM_DataModule::right_rpm_counter++;
 }
