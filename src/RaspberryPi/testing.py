4ee0bced1a7fb210fb792de7d40f065f1eb3e5de
Matthew Laptop <matthewal194@gmail.com>
2024-01-21 13:45:50 -0700

    Random stuff that doesn't really help anybody but I felt like doing it

--- src/RaspberryPi/testing.py
+++ src/RaspberryPi/testing.py
@@ -1,8 +1,29 @@
 import serial
 import serial.tools.list_ports
 import time
-
+# from xmodem import XMODEM
 # https://www.youtube.com/watch?v=jU_b8WBTUew <------ watch this video
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# def getc(size, timeout=1):
+#      return ser.read(size) or None
+
+# def putc(data, timeout=1):
+#      return ser.write(data)  # note that this ignores the timeout
+
+# modem = XMODEM(getc, putc)
+
+
 
 def get_port_paths():
     ports = list(serial.tools.list_ports.comports())
@@ -22,19 +43,21 @@ def get_serial_devices(port_paths):
         device.reset_input_buffer()
     return serial_devices
 
-def start_logging():
+def begin_logging(serial_devices):
     # send command to nano's to start logging onto their SD cards
-    pass
+    print("Begin logging")
 
-def end_logging():
+
+def end_logging(serial_devices):
     # send command to nano's to stop logging onto their SD cards
-    pass
+    print("End logging")
 
-def retrieve_logs():
+def retrieve_logs(serial_devices):
     # retrieve data from nano's SD cards
-    pass
+    print("Retrieving logs")
 
 def receive_data(serial_devices):
+    print("Receiving Data")
     try:
         while True:
             time.sleep(0.05)
@@ -48,10 +71,49 @@ def receive_data(serial_devices):
             ser.close()
         print("\nSerial Devices Closed")
 
+def get_input(serial_devices):
+    print_commands()
+    # Input loop
+    commands = {
+                'help': lambda : print_commands(),
+                'b': lambda : begin_logging(serial_devices),
+                'e': lambda : end_logging(serial_devices),
+                'r': lambda : retrieve_logs(serial_devices),
+                'q': lambda : exit_program(serial_devices)}
+
+    while True:   
+        command = str(input())
+        # I tried using a match statement here, but I couldn't so this'll have to do.
+        if command in commands:
+            commands[command]()
+        else:
+            print(f"{bcolors.WARNING}Warning: Command not found{bcolors.ENDC}")
+        time.sleep(.5)
+
+def print_commands():
+    print(f"{bcolors.BOLD}", end='')
+    print("Commands:")
+    print("help -- list of commands")
+    print("b -- begin logging")
+    print("e -- end logging")
+    print("r -- retreive logs")
+    print("q -- quit program")
+    print(f"{bcolors.ENDC}", end='')
+
+def exit_program(serial_devices):
+    for ser in serial_devices:
+        ser.close()
+        print("\nSerial Devices Closed")
+    print("Program exiting")
+    exit()
+
+
 def main():
     port_paths = get_port_paths()
     ser_devices = get_serial_devices(port_paths)
-    receive_data(ser_devices)
+
+    get_input(ser_devices)
+    # receive_data(ser_devices)
 
 if __name__ == "__main__":
     main()
