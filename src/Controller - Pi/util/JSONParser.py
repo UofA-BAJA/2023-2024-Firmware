962797e0f3e522b478514c05fe9b5cb11397d0b8
Matthew Laptop <matthewal194@gmail.com>
2024-02-11 20:28:37 -0700

    print JSON contents

--- src/Controller - Pi/util/JSONParser.py
+++ src/Controller - Pi/util/JSONParser.py
@@ -13,5 +13,8 @@ git_root = repo.git.rev_parse("--show-toplevel")
 # Construct the path to the file you want to reference
 file_path_in_git_root = Path(git_root) / 'src' / 'config.json'
 
+with open(file_path_in_git_root, 'r') as file:
+    data = json.load(file)
+print(data)
 
-print(file_path_in_git_root)
+# print(file_path_in_git_root)
