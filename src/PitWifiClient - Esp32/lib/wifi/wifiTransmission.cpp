68b5d406519713a1f8cd01b1260dc26352cf094a
B-i-x <bix.romero@gmail.com>
2024-03-05 12:54:50 -0700

    setting up folders

---
+++ src/PitWifiClient - Esp32/lib/wifi/wifiTransmission.cpp
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+#include <WiFiMulti.h>
+#include <WiFi.h>
+
+WiFiMulti WiFiMulti;
+
+// Use WiFiClient class to create TCP connections
+WiFiClient client;
+
+void initializeWifi() {
+    // We start by connecting to a WiFi network
+    WiFiMulti.addAP("yourAP", "yourPassword");
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Waiting for WiFi... ");
+
+    while(WiFiMulti.run() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(500);
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    delay(500);
+}
+
+void connectToHost() {
+    const uint16_t port = 80;
+    const char * host = "192.168.4.1"; // ip or dns
+    // const uint16_t port = 1337;
+    // const char * host = "192.168.1.10"; // ip or dns
+
+    Serial.print("Connecting to ");
+    Serial.println(host);
+
+
+
+    if (!client.connect(host, port)) {
+        Serial.println("Connection failed.");
+        Serial.println("Waiting 5 seconds before retrying...");
+        delay(5000);
+        return;
+    }
+}
+
+void printWirelessly(String str) { //ik i shouldnt use strings but i am lazy
+    client.print(str);
+}
+
+String readWirelesssSingleLine() {
+    int maxloops = 0;
+
+    //wait for the server's reply to become available
+    while (!client.available() && maxloops < 1000) {
+        maxloops++;
+        delay(1); //delay 1 msec
+    }
+
+    if (client.available() > 0) {
+        //read back one line from the server
+        String line = client.readStringUntil('\r');
+        return line;
+    } else {
+        Serial.println("client.available() timed out ");
+    }
+
+    return "";
+}
