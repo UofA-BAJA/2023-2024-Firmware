0e83501942692a0557f414d41029e85c5672c1cf
B-i-x <bix.romero@gmail.com>
2024-03-23 10:42:56 -0700

    maybe works?

--- src/DataModule - Nano/lib/analog/analog.h
+++ src/DataModule - Nano/lib/analog/analog.h
@@ -8,7 +8,6 @@ class BRK_DataModule : public BAJA_EMBEDDED::DataModule
     private:
         ;
     public:
-        int analogPin = 5; // ADC pin you're going to read
         void set_data_module_type() override;
         void data_module_setup_procedure() override;
         void data_module_logging_procedure() override;
