107a57a4c406acf5a0d0a2e546c849ffbdbc62b6
B-i-x <bix.romero@gmail.com>
2024-03-30 17:42:55 -0700

    getting rid of old stuff

--- examples/wifi/PitWifiServer - Esp32/lib/logic/logic.h
+++ examples/wifi/PitWifiServer - Esp32/lib/logic/logic.h
@@ -1,17 +1,6 @@
 #ifndef LOGIC_H
 #define LOGIC_H
-#include <Arduino.h>
 
 void operatingProcedure();
-
-bool waitForCommand(const char* cmmdString);
-
-void recvWithStartEndMarkers();
-
-bool getNextDevice(const char* messageBuffer, char* output, size_t outputSize);
-void setTextAfterHeader(char* buffer, size_t bufferSize, const char* header, const char* newMessage);
-void printTextAfterHeader(const char* buffer, const char* header);
-void setDeviceAndMessageInBufferTo(const char* device, const char* message);
-void resetMessageBuffer();
 
 #endif // LOGIC_H
